c73c0c9dcf222193531b0eace49347ecd398c244
nuts <nuts272@mail.ru>
2025-02-03 15:18:53 +0300

    add types CatMinInfo/ Cat

--- @types/common.d.ts
+++ @types/common.d.ts
@@ -8,4 +8,27 @@ export type CatMinInfo  = {
     name: string;
     description: string;
     gender: 'male' | 'female' | 'unisex';
-}
+};
+
+
+/**
+ * Полная информация о коте
+ * @property id - id
+ * @property name - имя
+ * @property description - описание
+ * @property tags - тэги
+ * @property gender - пол
+ * @property likes - количество лайков
+ * @property dislikes - количество дизлайков
+ * @property [message=] - сообщение
+ */
+export type Cat  = {
+    id: number;
+    name: string;
+    description: string;
+    tags: string;
+    gender: 'male' | 'female' | 'unisex';
+    likes: number;
+    dislikes: number;
+    message?: string;
+};
