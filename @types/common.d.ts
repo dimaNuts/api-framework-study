5078eca1a26d6fa50d146a5f1173aa858b36652f
nuts <nuts272@mail.ru>
2025-02-04 17:00:29 +0300

    add types FailAddCat/ CatsList

--- @types/common.d.ts
+++ @types/common.d.ts
@@ -32,3 +32,16 @@ export type Cat  = {
     dislikes: number;
     message?: string;
 };
+
+
+/**
+ * Объект может вернуть post метод [cats_add]{@link https://meowle.fintech-qa.ru/api/core/api-docs-ui/#/%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5/post_cats_add}
+ * При попытке создания котика с именем, которое уже есть
+ */
+export type FailAddCat  = { cats: { cat: CatMinInfo[]; errorDescription: string } };
+
+
+/**
+ *  Ответ при успешном добавлении кота
+ */
+export type CatsLIst = { cats: Cat[] };
