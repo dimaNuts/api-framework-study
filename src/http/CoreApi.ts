94db65ec93c7e30e101457d68ddd30c1f0fdbd6d
nuts <nuts272@mail.ru>
2025-02-05 18:26:55 +0300

    add method getCatById in CoreApi

--- src/http/CoreApi.ts
+++ src/http/CoreApi.ts
@@ -10,4 +10,19 @@ export default class CoreApi extends Client {
     // получении http клиента
     static coreApiHttpClient: AxiosInstance = Client.getInstance();
 
+    /**
+     * Get-Метод получения кота по id [get-by-id]{@link https://meowle.fintech-qa.ru/api/core/api-docs-ui/#/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA/get_cats_get_by_id}
+     * @param id - id
+     */
+    static async getCatById(id: number): Promise<AxiosResponse<{cat: Cat}>> {
+        let response: AxiosResponse;
+        try {
+            const params = new URLSearchParams({ id: id.toString() });
+            response = await this.coreApiHttpClient(`${this.api}/get-by-id/?${params}`);
+
+        } catch (error) {
+            console.error(error);
+        }
+        return response;
+    }
 };
