2e7467f13630a6e3954079b29dc7ca8d761cbd71
nuts <nuts272@mail.ru>
2025-02-05 18:08:48 +0300

    add coreApiHttpClient in CoreApi

--- src/http/CoreApi.ts
+++ src/http/CoreApi.ts
@@ -1,11 +1,13 @@
 import Client from './Client';
 import { URLSearchParams } from "node:url";
-import axios, { AxiosResponse } from "axios";
+import axios, {AxiosInstance, AxiosResponse} from "axios";
 import { CatMinInfo, Cat, CatsLIst, FailAddCat } from "../../@types/common";
 
 
 export default class CoreApi extends Client {
     // endpoint core Api
     static api: string = 'core/cats';
+    // получении http клиента
+    static coreApiHttpClient: AxiosInstance = Client.getInstance();
 
 };
