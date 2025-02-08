5e3137f159ad7d940a338e845371448eb8fd5cd6
nuts <nuts272@mail.ru>
2025-02-08 20:24:09 +0300

    Add allure mocha reference demo_test.ts

--- test/demo_test.ts
+++ test/demo_test.ts
@@ -1,12 +1,47 @@
-import { assert } from 'chai';
+import {assert} from 'chai';
 import CoreApi from '../src/http/CoreApi';
+import * as allure from "allure-js-commons";
+import {Severity} from "allure-js-commons";
 
-describe('Проверка функционала добавления котов', async () => {
-    it('Получение кота по id', async () => {
+describe('Проверка кота', async () => {
+    // Id идентификатор теста @allure.id:⟨VALUE⟩
+    it('Проверка имени кота по Id @allure.id: 123', async () => {
+        /*
+        * Metadata метаданные { @link https://allurereport.org/docs/mocha-reference/#metadata }
+         */
+        // Description описание теста
+        await allure.description("Этот тест проверяет соответствует ли имя кота с именем, полученным по id");
+        // Owner автор(владелец)
+        await allure.owner("Кот Матроскин");
+        // Tag тэг
+        await allure.tag("getNameById");
+        await allure.tags("getNameById", "getCatById");
+        // Severity серьезность теста
+        await allure.severity(Severity.CRITICAL);
+        // Label
+        await allure.label("feature", "");
+        // Link ссылка
+        await allure.link(
+            "https://meowle.fintech-qa.ru/api/core/api-docs-ui/#/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA/get_cats_get_by_id",
+            "Swagger UI"
+        );
+        await allure.link("https://meowle.fintech-qa.ru", "Meowle");
+
         const name = 'Бамбино';
+        // Parameter
+        await allure.parameter("time", new Date().toString(), { excluded: true });
+        await allure.parameter("name", name);
+        const id = 130185;
+        await allure.parameter("id", id.toString());
 
-        const response = await CoreApi.getCatById(130185);
+        const response = await CoreApi.getCatById(id);
+        await allure.logStep(`выполнен запрос GET /get-by-id c параметром ${id}`);
+        await allure.attachment(
+            'cat',
+            JSON.stringify(response.data.cat, null, 2),
+            'application/json'
+        );
 
         assert.equal(response.data.cat.name, name, 'Имена не соответствуют');
-    });
+    })
 })
